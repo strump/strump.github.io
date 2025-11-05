ca9bb2fe1bea559ba0f6b66fbd363e9e929a9f7a
S. Kozyr <s.trump@gmail.com>
2025-11-05 20:54:08 +0200

    Added keys quick filter field

--- translations_view/filters.js
+++ translations_view/filters.js
@@ -18,22 +18,33 @@ function update_columns_visibility() {
     document.getElementById("columns_style").innerHTML = css_code;
 }
 
-function update_tags_visibility() {
-    var unchecked = [];
+function update_rows_visibility() {
+    var keys_filter = get_keys_quick_filter_css();
+    var unchecked_tags = [];
     for(var inp of document.querySelectorAll(".filter_form .tag_check input")) {
         if (inp.checked) {
-            unchecked.push(inp.name.substr(4));
+            unchecked_tags.push(inp.name.substr(4));
         }
     }
     var css_code = ".translations tr {display:none;}\n" +
     ".translations tr.header {display:table-row;}\n";
-    for(var tag_id of unchecked) {
-        css_code += ".translations tr[data-twine-tags*=\","+tag_id+",\"] {\n" +
+    for(var tag_id of unchecked_tags) {
+        css_code += ".translations tr[data-twine-tags*=\","+tag_id+",\"]"+keys_filter+" {\n" +
                     "    display: table-row;\n" +
                     "}\n";
     }
 
-    document.getElementById("tags_style").innerHTML = css_code;
+    document.getElementById("rows_style").innerHTML = css_code;
+}
+
+function get_keys_quick_filter_css() {
+    var filter_input = document.getElementById("keys_quick_filter");
+    var search_text = filter_input.value.trim();
+    var css_code = "";
+    if (search_text.length > 0)
+        css_code = "[data-twine-key*=\""+search_text+"\"]";
+    return css_code;
+
 }
 
 function init_listeners() {
@@ -41,6 +52,8 @@ function init_listeners() {
         inp.addEventListener("change", update_columns_visibility);
     }
     for(var inp of document.querySelectorAll(".filter_form .tag_check input")) {
-        inp.addEventListener("change", update_tags_visibility);
+        inp.addEventListener("change", update_rows_visibility);
     }
+
+    document.getElementById("keys_quick_filter").addEventListener("input", update_rows_visibility);
 }
