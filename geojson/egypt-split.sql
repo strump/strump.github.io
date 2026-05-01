3943297bf0cd6009214dc53a47ffd7850ada92a7
S. Kozyr <s.trump@gmail.com>
2026-05-01 11:00:07 +0300

    Added third option of Egypt split

--- geojson/egypt-split.sql
+++ geojson/egypt-split.sql
@@ -232,3 +232,78 @@ select mwm_group, mwm_group as osm_id, mwm_group as label, '#DAA520' as fill, (n
 from objects_by_region
 
 ) TO 'borders_split_2.geojson' WITH (FORMAT GDAL, DRIVER GeoJSON, LAYER_CREATION_OPTIONS 'ID_FIELD=id');
+
+-----------------
+-- New regions V3
+
+DROP TABLE IF EXISTS region_to_mwm;
+CREATE TABLE region_to_mwm AS (
+  SELECT *
+    FROM VALUES
+    ('3060792', 'Zone A'),
+    ('3060793', 'Zone A'),
+    ('3061757', 'Zone C'),
+    ('3061758', 'Zone C'),
+    ('3061826', 'Zone A'),
+    ('3061827', 'Zone C'),
+    ('3061846', 'Zone A'),
+    ('3062184', 'Zone A'),
+    ('3062185', 'Zone B'),
+    ('3584607', 'Zone A'),
+    ('3726124', 'Zone B'),
+    ('3726170', 'Zone C'),
+    ('3726175', 'Zone C'),
+    ('3726184', 'Zone C'),
+    ('3726186', 'Zone C'),
+    ('3726189', 'Zone C'),
+    ('3726211', 'Zone C'),
+    ('3824206', 'Zone B'),
+    ('3824207', 'Zone A'),
+    ('3824513', 'Zone A'),
+    ('4103336', 'Zone B'),
+    ('4103337', 'Zone B'),
+    ('4103403', 'Zone A'),
+    ('4103404', 'Zone A'),
+    ('4103405', 'Zone A'),
+    ('4103406', 'Zone A'),
+    ('4103407', 'Zone A')
+  t(region_id, mwm_group)
+);
+
+-- To GeoJson
+
+COPY (
+
+WITH top_egypt_relation as (
+ select id, unnest(refs) as child_ref, unnest(ref_roles) as role
+   from osm_egypt
+  where id = 1473947
+),
+egypt_region_ids as (
+ select child_ref as region_id
+   from top_egypt_relation
+  where role = 'subarea'
+),
+egypt_regions as (
+ select region_to_mwm.mwm_group, ST_MemUnion_Agg(OSM.geometry) as geometry
+   from '/Users/skozyr/Projects/OSM/duckdb/egypt-260420.osm.pbf' OSM
+   JOIN region_to_mwm ON OSM.id = region_to_mwm.region_id
+  GROUP BY region_to_mwm.mwm_group
+),
+objects as (
+ select id, geometry
+   from '/Users/skozyr/Projects/OSM/duckdb/egypt-260420.osm.pbf'
+  where kind in ('line', 'node', 'area')
+),
+objects_by_region as (
+SELECT egypt_regions.mwm_group, count(objects.*) as num_objects, egypt_regions.geometry
+  FROM egypt_regions
+  JOIN objects on ST_Intersects(egypt_regions.geometry, objects.geometry)
+ GROUP BY egypt_regions.mwm_group, egypt_regions.geometry
+)
+
+select mwm_group, mwm_group as osm_id, mwm_group as label, '#B7410E' as fill, (num_objects*0.8/2389655) as "fill-opacity",
+       num_objects, round(100 * num_objects / 5716441, 0) as percent, geometry
+from objects_by_region
+
+) TO 'borders_split_3.geojson' WITH (FORMAT GDAL, DRIVER GeoJSON, LAYER_CREATION_OPTIONS 'ID_FIELD=id');
